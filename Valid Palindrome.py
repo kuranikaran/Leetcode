b9469cd2808e3592115c84858e41ec5e3660d7fe
Karan Kurani <85026744+kuranikaran@users.noreply.github.com>
2026-01-20 22:34:22 +0530

    Create Valid Palindrome.py

---
+++ Valid Palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = []
+        for i in s:
+            if i.isalnum():
+                new.append(i.lower())
+        return new == new[::-1]
