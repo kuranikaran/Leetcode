5df905e5a627320f41065e8acdbccc376c0384ab
Karan Kurani <85026744+kuranikaran@users.noreply.github.com>
2026-01-20 22:25:34 +0530

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,5 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+        
+------------------------------------------------------------------------
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
